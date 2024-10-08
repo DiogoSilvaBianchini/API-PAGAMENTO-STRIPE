f56673b6c36b95b6dafcea57e22a671126b7f509
unknown <diogosilvabianchini@gmail.com>
2024-10-08 09:11:49 -0300

    Configuração inicial do servidor.

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+
+const app = express()
+const PORT = process.env.PORT || 8082
+
+app.use(morgan("dev"))
+
+
+app.listen(PORT, () => console.log("ok"))
